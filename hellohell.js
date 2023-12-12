98e443da250e5e571f0e5b5f10a458e7b038c082
Onkar <onkarvibhute2905@gmail.com>
2023-12-12 16:03:16 +0530

    first

--- hellohell.js
+++
@@ -1,4 +0,0 @@
-console.log("welocome to hell");
-console.log("line 2");
-console.log("line 3");
-
---
+++ hh.js
@@ -0,0 +1,5 @@
+console.log("hi");
+console.log("line2");
+console.log("line 3");
+hdhdhdhdhd
+yddhdhdj
